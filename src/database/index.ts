68566f5430a45e6e5868415ea0aae3a40ac84531
r-o-k-u <corneliusokaya@gmail.com>
2022-11-11 14:24:05 +0300

    update

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,18 @@ const dbUser = Locals.config().dbUser as string;
 const dbHost = Locals.config().dbHost;
 const dbDriver = Locals.config().dbDriver;
 const dbPassword = Locals.config().dbPassword;
-//console.log(Locals.config());
+console.log(
+  "dbName",
+  dbName,
+  "dbUser",
+  dbUser,
+  "dbHost",
+  dbHost,
+  "dbDriver",
+  dbDriver,
+  "dbPassword",
+  dbPassword
+);
 import Log from "../utils/Log";
 
 /**
